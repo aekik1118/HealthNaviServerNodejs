776f8faaace1809c5e3b9a8a475c771b555a8879
WON <aekik1118@naver.com>
2018-11-27 23:30:11 +0900

    init

---
+++ reservation.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var FitnessEquipments = [0,0,0,0,0,0];
+
+io.on('connection', function(socket) {
+  console.log("user connect");
+
+  socket.on('checkIn', function(){
+    console.log("checkIn");
+    FitnessEquipments[0] = (FitnessEquipments[0]-1)*-1;
+    console.log(FitnessEquipments[0]);
+    io.emit('checkOn', FitnessEquipments[0]);
+  });
+
+  socket.on('checkOut', function(){
+    console.log("checkOut");
+    io.emit('checkOut', "Out");
+  });
+
+});
+
+//서버를 시작한다.
+http.listen(9000, function(){
+  console.log("server on 9000");
+});
